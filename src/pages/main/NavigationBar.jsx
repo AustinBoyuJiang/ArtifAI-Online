066464578762951b79e352870f4b6f2798221041
Game Development Club WVSS <austinjiangboyu@gmail.com>
2024-04-12 23:13:40 -0700

    Update

--- src/pages/main/NavigationBar.jsx
+++ src/pages/main/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles/NavigationBar.css';
+import Icon from "./assets/icon.png"
 
 export default function NavigationBar() {
     const [isVisible, setIsVisible] = useState(true);
@@ -20,7 +21,8 @@ export default function NavigationBar() {
 
     return (
         <div className={`navigation-bar ${isVisible ? '' : 'navigation-bar-hidden'}`}>
-            <span className="navbar-logo">ArtifAI</span>
+            <img src={Icon} className="navbar-logo-icon" alt="Logo" href='/'/>
+            <span className="navbar-logo-text">ArtifAI</span>
             <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link-active' : '')}>HOME</NavLink>
             <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-link-active' : '')}>ABOUT</NavLink>
             <NavLink to="/faq" className={({ isActive }) => (isActive ? 'nav-link-active' : '')}>FAQ</NavLink>
